a37da50c8c14c6e898bc2eb11b4e4fd5a990d206
Apasoft Training <54261225+ApasoftTraining@users.noreply.github.com>
2024-08-01 00:30:08 +0200

    ate

--- view_machine_data.py
+++ view_machine_data.py
@@ -1,8 +1,10 @@
 import socket
+import os
 
 def main():
     hostname = socket.gethostname()
     print(f"Name of the server: {hostname}")
+    print(f'The currect direcory is: {os.getcwd()}')
 
 if __name__ == "__main__":
     main()
