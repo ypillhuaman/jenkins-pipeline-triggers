2bfb53bed9e81edc5fb606e0663414cfd86288ef
ypillhuaman <79329588+ypillhuaman@users.noreply.github.com>
2025-01-08 12:23:37 -0500

    Update view_machine_data.py

--- view_machine_data.py
+++ view_machine_data.py
@@ -5,6 +5,7 @@ def main():
     hostname = socket.gethostname()
     print(f"Name of the server: {hostname}")
     print(f'The current directory is: {os.getcwd()}')
+    print(f'xoxoxoxoxxoxoxox')
 
 if __name__ == "__main__":
     main()
